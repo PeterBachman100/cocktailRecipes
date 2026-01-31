31b0a7726e5564e5f1baf75622f3b8428b9629f4
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2026-01-30 21:24:48 -0800

    Recipe Cards styling

--- frontend/src/components/recipeCard.jsx
+++ frontend/src/components/recipeCard.jsx
@@ -25,22 +25,91 @@ function RecipeCard({ recipeId }) {
 
   if (loading) return <p>Loading</p>;
 
-  let renderedSpirits = recipe.spirits.map((spirit) => <span key={spirit} style={{textTransform: 'uppercase', fontSize: '0.75rem', backgroundColor: '#fdcc82ff', color: '#373737ff', padding: '0.1rem 0.5rem', borderRadius: '1rem' }}>{ spirit }</span>);
-  let renderedFlavors = recipe.flavors.map((flavor) => <span style={{textTransform: 'capitalize', fontWeight: '500'}} key={flavor}><Leaf size={12} color='#499144ff' strokeWidth={3} /> {flavor}</span>);
+    const badgeStyle = {
+        textTransform: 'uppercase',
+        fontSize: '0.75rem',
+        backgroundColor: '#fdcc82ff',
+        color: '#373737ff',
+        padding: '0.1rem 0.5rem',
+        borderRadius: '1rem',
+    };
+
+    const flavorStyle = {
+        textTransform: 'capitalize',
+        fontWeight: '500',
+    };
+
+  let renderedSpirits = recipe.spirits.map((spirit) => <span key={spirit} style={badgeStyle}>{ spirit }</span>);
+  let renderedFlavors = recipe.flavors.map((flavor) => <span style={flavorStyle} key={flavor}><Leaf size={12} color='#499144ff' strokeWidth={3} /> {flavor}</span>);
   
   return (
-    <div style={{display: 'flex', flexDirection: 'column', padding: '1rem', gap: '1rem', borderRadius: '1rem', boxShadow: '#5a5a6e33 0px 7px 29px 0px', backgroundColor: '#f7f7f7ff'}}>  
-        <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '0.5rem', width: '100%'}}>
-            <img style={{width: '100%', borderRadius: '5%'}} src={recipe.image} />
-            <div style={{padding: '0.5rem', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                <h2 style={{textTransform: 'uppercase', fontSize: '1.5rem', fontWeight: '400', fontFamily: 'sans-serif', letterSpacing: '1.5px', lineHeight: '90%'}}>{recipe.title}</h2>
-                <div style={{display: 'flex', gap: '0.5rem'}}>{renderedSpirits}</div>
-                <div style={{display: 'flex', flexWrap: 'wrap', gap: '0.5rem'}}>{renderedFlavors}</div>  
+    <div 
+        style={{
+        display: 'flex', 
+        flexDirection: 'column', 
+        padding: '1rem', 
+        gap: '1rem', 
+        borderRadius: '1rem', 
+        boxShadow: '#5a5a6e33 0px 7px 29px 0px', 
+        backgroundColor: '#f7f7f7ff'
+    }}>  
+        <div style={{
+            display: 'grid', 
+            gridTemplateColumns: '1fr 2fr', 
+            gap: '0.5rem', 
+            width: '100%'
+        }}>
+            <div style={{position: 'relative'}}>
+                <img style={{width: '100%', borderRadius: '5%'}} src={recipe.image} />
+            </div>
+            <div style={{
+                padding: '0.5rem', 
+                display: 'flex', 
+                flexDirection: 'column', 
+                justifyContent: 'space-between'
+            }}>
+                <div style={{display: 'flex', gap: '0.5rem', justifyContent: 'space-between'}}>
+                    <h2 style={{
+                        textTransform: 'uppercase', 
+                        fontSize: '1.5rem', 
+                        fontWeight: '400', 
+                        fontFamily: 'sans-serif',
+                        letterSpacing: '1.5px', 
+                        lineHeight: '90%'
+                    }}>
+                        {recipe.title}
+                    </h2>
+                    <HeartPlus style={{width: '3rem'}} />
+                </div>
+                <div style={{
+                    display: 'flex', 
+                    gap: '0.5rem'
+                }}>
+                    {renderedSpirits}
+                </div>
+                <div style={{
+                    display: 'flex', 
+                    flexWrap: 'wrap', 
+                    gap: '0.5rem'
+                }}>
+                    {renderedFlavors}
+                </div>  
             </div>
         </div>
-        <div style={{display: 'flex', flexDirection: 'row', gap: '0.5rem'}}>
-            <div style={{fontWeight: '100', color: '#5b5a5aff', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '3', overflow: 'hidden'}}>{recipe.description}</div>
-            <div style={{display: 'flex', flexDirection: 'column', gap: '0.5rem'}}><HeartPlus size={20} /><Folder size={20} /></div>
+        <div style={{
+            display: 'flex', 
+            gap: '0.5rem'
+        }}>
+            <div style={{
+                fontWeight: '100', 
+                color: '#5b5a5aff', 
+                display: '-webkit-box', 
+                WebkitBoxOrient: 'vertical', 
+                WebkitLineClamp: '3', 
+                overflow: 'hidden'
+            }}>
+                {recipe.description}
+            </div>
         </div>
     </div>
   );
