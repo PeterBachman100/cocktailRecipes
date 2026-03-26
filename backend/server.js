31fc8cbb8bb506974261d3ff2a799486ee588455
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2026-03-26 09:55:16 -0700

    hot fix

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ app.use('/api/folders', folderRoutes);
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) =>
+  app.get('/{*splat}', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
